15098cd3bf9668f0502a926a87838f78316a43bc
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-11-01 17:51:10 -0500

    Navbar on left size

---
+++ portfolio/src/app/navbar/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+  {
+    routerlink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard',
+  },
+  {
+    routerlink: 'products',
+    icon: 'fal fa-box-open',
+    label: 'Products',
+  },
+  {
+    routerlink: 'statistics',
+    icon: 'fal fa-chart-bar',
+    label: 'Statistics',
+  },
+  {
+    routerlink: 'coupens',
+    icon: 'fal fa-tags',
+    label: 'Coupens',
+  },
+  {
+    routerlink: 'pages',
+    icon: 'fal fa-file',
+    label: 'Pages',
+  },
+  {
+    routerlink: 'media',
+    icon: 'fal fa-camera',
+    label: 'Media',
+  },
+  {
+    routerlink: 'settings',
+    icon: 'fal fa-cog',
+    label: 'Settings',
+  },
+];
--- portfolio/src/app/navbar/navbar.component.ts
+++ portfolio/src/app/navbar/navbar.component.ts
@@ -1,16 +1,126 @@
-import { Component } from '@angular/core';
-import { DarkModeService } from '../_services/dark-mode.service';
+// import { Component } from '@angular/core';
+// import { DarkModeService } from '../_services/dark-mode.service';
+// import { NavigationEnd, Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-navbar',
+//   templateUrl: './navbar.component.html',
+//   styleUrls: ['./navbar.component.scss'],
+// })
+// export class NavbarComponent {
+//   constructor(
+//     public darkModeService: DarkModeService,
+//     private router: Router
+//   ) {}
+
+//   toggleDarkMode() {
+//     this.darkModeService.toggleDarkMode();
+//     this.router.events.subscribe((event) => {
+//       if (event instanceof NavigationEnd) {
+//         this.currentRoute = event.urlAfterRedirects.substring(1);
+//       }
+//     });
+//   }
+
+//   isMenuOpen: boolean = false;
+
+//   isResponsive: boolean = false;
+//   currentRoute: string = '';
+
+//   ngOnInit() {}
+
+//   toggleResponsive() {
+//     this.isResponsive = !this.isResponsive;
+//   }
+// }
+
+
+
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { navbarData } from './nav-data';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
-  selector: "app-navbar",
-  templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.scss"],
+  selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('350ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('350ms', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate(
+          '1000ms',
+          keyframes([
+            style({ transform: 'rotate(0deg)', offset: '0' }),
+            style({ transform: 'rotate(2turn)', offset: '1' }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
-export class NavbarComponent {
-  constructor(public darkModeService: DarkModeService) {}
+export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  toggleDarkMode() {
-    this.darkModeService.toggleDarkMode();
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth = 0;
+  navData = navbarData;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth <= 768) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({
+        collapsed: this.collapsed,
+        screenWidth: this.screenWidth,
+      });
+    }
   }
-  
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
 }
