aa7aefc5820d1442ff0c01dd358f49fcc9700643
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-11-03 13:27:50 -0500

    Added all the basic details from existing project

--- portfolio/src/app/_services/dark-mode.service.ts
+++ portfolio/src/app/_services/dark-mode.service.ts
@@ -11,13 +11,13 @@ export class DarkModeService {
   toggleDarkMode(): void {
     const root = document.documentElement;
     const isDarkMode =
-      root.style.getPropertyValue("--background-color") === "#000";
+      root.style.getPropertyValue('--background-color') === '#272935';
 
     if (isDarkMode) {
       root.style.setProperty("--background-color", "white");
       root.style.setProperty("--font-color", "black");
     } else {
-      root.style.setProperty("--background-color", "#000");
+      root.style.setProperty('--background-color', '#272935');
       root.style.setProperty("--font-color", "white");
     }
   }
