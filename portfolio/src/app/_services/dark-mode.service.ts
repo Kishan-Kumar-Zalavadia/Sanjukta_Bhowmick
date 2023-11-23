1765654635825f5cd4a75535fc9176e4835908e6
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-11-23 15:29:10 -0600

    Glass effect for project-card, changed backgound image

--- portfolio/src/app/_services/dark-mode.service.ts
+++ portfolio/src/app/_services/dark-mode.service.ts
@@ -10,15 +10,19 @@ export class DarkModeService {
 
   toggleDarkMode(): void {
     const root = document.documentElement;
-    const isDarkMode =
-      root.style.getPropertyValue('--background-color') === '#272935';
+    const isDarkMode = root.style.getPropertyValue('--background-color') === '#272935';
+    const bg_dark = window.getComputedStyle(root).getPropertyValue('--bg-dark');
+    const bg_light = window.getComputedStyle(root).getPropertyValue('--bg-light');
 
     if (isDarkMode) {
       root.style.setProperty("--background-color", "white");
       root.style.setProperty("--font-color", "black");
+      root.style.setProperty('--bg', bg_light);
+
     } else {
       root.style.setProperty('--background-color', '#272935');
       root.style.setProperty("--font-color", "white");
+      root.style.setProperty('--bg', bg_dark);
     }
   }
 }
