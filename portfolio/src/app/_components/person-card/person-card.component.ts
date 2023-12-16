fb2a4a81b0b00af501726a8866ef768c3427d6d8
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-12-16 09:41:05 -0600

    Added person-details

--- portfolio/src/app/_components/person-card/person-card.component.ts
+++ portfolio/src/app/_components/person-card/person-card.component.ts
@@ -11,4 +11,13 @@ export class PersonCardComponent {
   @Input() person!: People;
 
   constructor(private router: Router) {}
+
+  //* ------------------------------------------------------------------------------------------------
+  //* redirect to person's website
+  redirectToPersonDetails(){
+    console.log(this.person.url);
+    if (this.person.url) {
+      window.open(this.person.url, '_blank');
+    }
+  }
 }
