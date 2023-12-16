dd4cd14570fc25d4ccedb9696e7e69679e48716c
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-12-16 10:33:24 -0600

    Created a separate file to store personal information

---
+++ portfolio/src/app/_DATA/details-data.ts
@@ -0,0 +1,53 @@
+import { Details } from '../_model/details';
+// details.data.ts
+
+export const sampleDetails: Details = {
+  //* Title
+  profession_title: 'Research Associate Professor',
+
+  //* Work Location
+  work_location:
+    'Department of Computer Science and Engineering, University of North Texas',
+
+  //* GitHub link
+  github_link: 'https://github.com/SanjuktaBhowmick/ESSENS',
+
+  //* Google Scholar link
+  google_scholar_link:
+    'https://scholar.google.com/citations?user=MegF9mYAAAAJ&hl=en',
+
+  //* CV link
+  cv_link: 'http://localhost:4200/assets/cv.pdf',
+
+  //* Description Paragraph-1
+  description_paragraph_1:
+    'I am a research associate professor at the CSE department at UNT. I love all things graphs.',
+
+  //* Description Paragraph-2
+  description_paragraph_2:
+    'My research is on studying the properties of large, dynamic complex networks and using high performance computing to analyze them. I am particularly interested in understanding how noise affects network analysis, and developing uncertainty quantification for network analysis.',
+
+  //* Email Address
+  email: 'sanjukta.bhowmick@unt.edu',
+
+  //* Phone Number
+  phone: '+1 814-880-6875',
+
+  //* Office / Room number
+  place: 'F291 Discovery Park, Denton, TX, 76201, United States',
+
+  //* Office Timing - 1
+  officeHours_1: 'Monday: 12:30 to 13:30',
+
+  //* Office Timing - 2
+  officeHours_2: 'Wednesday: 13:00 to 14:00',
+
+  //* Office Timing - 3
+  officeHours_3: undefined,
+
+  //* Office Timing - 4
+  officeHours_4: undefined,
+
+  //* Office Timing - 5
+  officeHours_5: undefined,
+};
---
+++ portfolio/src/app/_model/details.ts
@@ -0,0 +1,22 @@
+export interface Details {
+    
+  profession_title?: string;
+  work_location?: string;
+
+  github_link?: string;
+  google_scholar_link?: string;
+  cv_link?: string;
+
+  description_paragraph_1?: string;
+  description_paragraph_2?: string;
+
+  email?: string;
+  phone?: string;
+  place?: string;
+
+  officeHours_1?: string;
+  officeHours_2?: string;
+  officeHours_3?: string;
+  officeHours_4?: string;
+  officeHours_5?: string;
+}
