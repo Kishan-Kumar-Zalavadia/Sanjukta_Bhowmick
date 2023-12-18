3ddfb4893f88bcefa1f36cd995669713939a8751
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-12-18 15:05:22 -0600

    Re-order projects

--- portfolio/src/app/_DATA/project-data.ts
+++ portfolio/src/app/_DATA/project-data.ts
@@ -3,6 +3,29 @@ import { Project } from '../_model/project';
 export const PROJECTS: Project[] = [
   {
     id: 1,
+    image: '../../assets/project_images/project_3.png',
+    name: 'Dynamic Network Algorithms',
+    description:
+      'Network dynamics involves studying how the properties of networks change as their structure evolves. In this project we aim to develop a template for creating parallel scalable algorithms for updating different properties of large-scale dynamic networks.Our goal is to move from the current approach of case by case algorithm design to an wholistic support infrastructure for dynamic network analysis. This project is funded by NSF and is in collaboration with Dr. Sajal Das a and Dr. Boyana Norris.Another aspect of dynamic networks is to create the graph by observing temporal events. This work is in collaboration with Dr. Jeremy Wendt, Dr. Richard Field, Dr. Cynthia Phillips, and Dr. Sucheta Soundarajan',
+    publicationIDs: [8, 9, 10],
+  },
+  {
+    id: 2,
+    image: '../../assets/project_images/project_4.png',
+    name: 'Modeling NonDeterminism in Exascale Applications',
+    description:
+      'The convergence of extremely high levels of hardware concurrency and the effective overlap of computation and communication in asynchronous executions are resulting in growing nondeterminism in High Performance Computing (HPC) applications. In this project we aim to model nondeterministic executions by determining nondeterministic motifs in executions of HPC applications and using graph theory to categorize and compare the motifs to study the impact of non-determinism. This project is funded by NSF and is in collaboration with Dr. Michela Taufer and Dr. Heike Jagode .',
+  },
+  {
+    id: 3,
+    image: '../../assets/project_images/project_5.png',
+    name: 'Multilayer Network Analysis',
+    description:
+      'Multilayer networks model systems that are related via multiple features and/or are formed of multiple types of entities. We are developing a decoupling based method for efficiently analyzing multilayer networks. Using decoupling component networks can be analyzed independently and then the analysis results can be combined to produce the final results. This work is in collaboration with Dr. Sharma Chakravarthy.',
+    publicationIDs: [11, 12, 13],
+  },
+  {
+    id: 4,
     image: '../../assets/project_images/project_1.png',
     name: 'Analyzing Noisy Networks',
     date: '2001-01-01',
@@ -11,35 +34,12 @@ export const PROJECTS: Project[] = [
     publicationIDs: [1, 2, 3, 4],
   },
   {
-    id: 2,
+    id: 5,
     image: '../../assets/project_images/project_2.png',
     name: 'Applications of Network Science',
     description:
       'Networks model the complex interactions between entities in many diverse disciplines. The properties of the networks provide insight to characteristics of the underlying complex system. We have used complex networks in many different areas including software engineering with Dr. Harvey Siy, bioinformatics with Dr. Hesham Ali and Dr. Kate Cooper, epidemiology with Dr. Armin Mikler , and narrative networks to study interactions between characters in plays.',
     publicationIDs: [5, 6, 7],
-  },
-  {
-    id: 3,
-    image: '../../assets/project_images/project_3.png',
-    name: 'Dynamic Network Algorithms',
-    description:
-      'Network dynamics involves studying how the properties of networks change as their structure evolves. In this project we aim to develop a template for creating parallel scalable algorithms for updating different properties of large-scale dynamic networks.Our goal is to move from the current approach of case by case algorithm design to an wholistic support infrastructure for dynamic network analysis. This project is funded by NSF and is in collaboration with Dr. Sajal Das a and Dr. Boyana Norris.Another aspect of dynamic networks is to create the graph by observing temporal events. This work is in collaboration with Dr. Jeremy Wendt, Dr. Richard Field, Dr. Cynthia Phillips, and Dr. Sucheta Soundarajan',
-    publicationIDs: [8, 9, 10],
-  },
-  {
-    id: 4,
-    image: '../../assets/project_images/project_4.png',
-    name: 'Modeling NonDeterminism in Exascale Applications',
-    description:
-      'The convergence of extremely high levels of hardware concurrency and the effective overlap of computation and communication in asynchronous executions are resulting in growing nondeterminism in High Performance Computing (HPC) applications. In this project we aim to model nondeterministic executions by determining nondeterministic motifs in executions of HPC applications and using graph theory to categorize and compare the motifs to study the impact of non-determinism. This project is funded by NSF and is in collaboration with Dr. Michela Taufer and Dr. Heike Jagode .',
-  },
-  {
-    id: 5,
-    image: '../../assets/project_images/project_5.png',
-    name: 'Multilayer Network Analysis',
-    description:
-      'Multilayer networks model systems that are related via multiple features and/or are formed of multiple types of entities. We are developing a decoupling based method for efficiently analyzing multilayer networks. Using decoupling component networks can be analyzed independently and then the analysis results can be combined to produce the final results. This work is in collaboration with Dr. Sharma Chakravarthy.',
-    publicationIDs: [11, 12, 13],
   },
   {
     id: 6,
