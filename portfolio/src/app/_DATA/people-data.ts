46e37590339342575ee56f86cce90e06b57fb9dc
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-12-19 16:24:08 -0600

    Added a button for website of person

--- portfolio/src/app/_DATA/people-data.ts
+++ portfolio/src/app/_DATA/people-data.ts
@@ -18,7 +18,7 @@ export const PEOPLE: People[] = [
     id: 3,
     name: 'Person 2',
     image: '../../assets/images/Eshaan.png',
-    url: undefined,
+    url: 'https://scholar.google.com/citations?view_op=list_works&hl=en&hl=en&user=MegF9mYAAAAJ&pagesize=80&sortby=pubdate',
     details:
       'Hey! I am Eshaan Singh and I am currently part of the TAMS program as a high school student. My research falls under the hypergraph domain in graph theory. Currently, I am working to develop a novel dynamic algorithm to classify hyperedges over time accurately.',
   },
