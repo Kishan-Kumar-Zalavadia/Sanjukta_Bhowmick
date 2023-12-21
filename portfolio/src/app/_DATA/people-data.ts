9e48ab1073b553def2330adf2f7ca496ae313354
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-12-21 08:56:09 -0600

    Updated the person data

--- portfolio/src/app/_DATA/people-data.ts
+++ portfolio/src/app/_DATA/people-data.ts
@@ -5,8 +5,9 @@ export const PEOPLE: People[] = [
     id: 1,
     name: 'Ali Khan',
     image: '../../assets/images/ali_khan.jpeg',
-    url: undefined,
-    details: '',
+    url: 'https://nam04.safelinks.protection.outlook.com/?url=https%3A%2F%2Fayk.dev%2F&data=05%7C02%7CKishanKumarZalavadia%40my.unt.edu%7Ce99f030259554b27a3ea08dc0194fd35%7C70de199207c6480fa318a1afcba03983%7C0%7C0%7C638386986640477467%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=nA%2B%2FWSYToSkoIVkP2XRV9QO9yHEU3Gm7Na7KfAml4Rs%3D&reserved=0',
+    details:
+      'I am a second year PhD Student. My research interest lies in the union of graph theory, machine learning, and scalable algorithms. My research focuses on developing neuro-symbolic algorithms for network comparison at scale.',
   },
   {
     id: 2,
@@ -21,5 +22,13 @@ export const PEOPLE: People[] = [
     url: undefined,
     details:
       'Hey! I am Eshaan Singh and I am currently part of the TAMS program as a high school student. My research falls under the hypergraph domain in graph theory. Currently, I am working to develop a novel dynamic algorithm to classify hyperedges over time accurately.',
+  },
+  {
+    id: 4,
+    name: 'Fariba Afrin Irany',
+    image: '../../assets/images/Fariba.png',
+    url: undefined,
+    details:
+      'I am a Ph.D. student at the Department of Computer Science and Engineering at the University of North Texas. My research focuses on developing techniques for analyzing multi-featured datasets. I have worked on algorithms for sampling large networks, perturbing contact networks to reduce the spread of infection, generating layers for multilayer networks, and analyzing data in a homogenous multilayer network. ',
   },
 ];
